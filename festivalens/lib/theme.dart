251e3cf0d90f3c2cb0d162b0c06711597d00d5b4
WillMBT <125831631+WillMBT@users.noreply.github.com>
2024-09-26 15:06:00 +1200

    Update theme.dart

--- festivalens/lib/theme.dart
+++ festivalens/lib/theme.dart
@@ -1,27 +1,27 @@
 import 'package:flutter/material.dart';
 
-final dcolor = Color(0xff2B2D42);  // Dark Color
-final dcolor2 = Color(0xffF7EBEC);  // Light Color
-final dcolor3 = Color(0xffDDBDD5);  // Accent Color
-final dcolor4 = Color(0xff7FD1B9);  // Secondary Accent Color
-final dcolor5 = Color(0xffFAB378);  // Additional Accent Color
+final darkcolor1 = Color(0xff2B2D42);  // Dark Color
+final darkcolor2 = Color(0xffF7EBEC);  // Light Color
+final darkcolor3 = Color(0xffDDBDD5);  // Accent Color
+final darkcolor4 = Color(0xff7FD1B9);  // Secondary Accent Color
+final darkcolor5 = Color(0xffFAB378);  // Additional Accent Color
 
-final lcolor = Color(0xF5F5F5F5);  // Light Color
-final lcolor2 = Color(0xff333333);  // Dark Color
-final lcolor3 = Color(0xff7B68EE);  // Accent Color
-final lcolor4 = Color(0xff48D1CC);  // Secondary Accent Color
-final lcolor5 = Color(0xffFF8C00);  // Additional Accent Color
+final lightcolor1 = Color(0xF5F5F5F5);  // Light Color
+final lightcolor2 = Color(0xff333333);  // Dark Color
+final lightcolor3 = Color(0xff7B68EE);  // Accent Color
+final lightcolor4 = Color(0xff48D1CC);  // Secondary Accent Color
+final lightcolor5 = Color(0xffFF8C00);  // Additional Accent Color
 
 
 
 final ThemeData darkTheme = ThemeData(
   brightness: Brightness.dark,
   colorScheme: ColorScheme.dark(
-  surface: dcolor,
-  onSurface: dcolor2,
-  primary: dcolor3,
-  secondary: dcolor4,
-  tertiary: dcolor5,
+  surface: darkcolor1,
+  onSurface: darkcolor2,
+  primary: darkcolor3,
+  secondary: darkcolor4,
+  tertiary: darkcolor5,
   ),
 );
 
@@ -29,10 +29,10 @@ final ThemeData darkTheme = ThemeData(
 final ThemeData lightTheme = ThemeData(
   brightness: Brightness.light,
   colorScheme: ColorScheme.light(
-  surface: lcolor,
-  onSurface: lcolor2,
-  primary: lcolor3,
-  secondary: lcolor4,
-  tertiary: lcolor5,
+  surface: lightcolor,
+  onSurface: lightcolor2,
+  primary: lightcolor3,
+  secondary: lightcolor4,
+  tertiary: lightcolor5,
   ),
 );
